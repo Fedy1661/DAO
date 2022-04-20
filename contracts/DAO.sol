50955473968b90ccd35f7aa5565ed792f52cd382
Fedor Konovalov <fedykonovalov@gmail.com>
2022-04-20 10:08:05 +0300

    Add DAO, Token smart-contracts Add tests for DAO Update libraries Import chai-bignumber Delete tests with Greeter.sol Delete Greeter.sol Add singleQuote to .prettierrc

---
+++ contracts/DAO.sol
@@ -0,0 +1,150 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DAO {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _voteCounter;
+
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable voteToken;
+    address public immutable owner;
+    address public immutable chairperson;
+    uint256 public minimumQuorum;
+    uint256 public debatingPeriodDuration;
+
+    struct Proposal {
+        address recipient;
+        string description;
+        uint256 finishAt;
+        uint256 pros;
+        uint256 cons;
+        mapping(address => bool) voted;
+        bool active;
+    }
+
+    struct Elector {
+        uint256 balance;
+        uint256 canClaimAt;
+    }
+
+    mapping(uint256 => Proposal) private _proposals;
+    mapping(uint256 => bytes) private _callDataProposals;
+    mapping(address => Elector) private _electors;
+
+    event Withdraw(address elector, uint256 amount);
+    event Deposit(address elector, uint256 amount);
+    event Vote(uint256 id, address elector, uint256 amount, bool support);
+    event FinishProposal(uint256 id, uint256 pros, uint256 cons, uint256 total, bool status);
+
+    modifier onlyChairperson {
+        require(msg.sender == chairperson, "Caller is not the chairperson");
+        _;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+    /**
+    * @param _chairperson Address chairman
+    * @
+    */
+    constructor(
+        address _chairperson,
+        address _voteToken,
+        uint256 _minimumQuorum,
+        uint256 _debatingPeriodDuration
+    ) {
+        owner = msg.sender;
+        chairperson = _chairperson;
+        voteToken = IERC20(_voteToken);
+        minimumQuorum = _minimumQuorum;
+        debatingPeriodDuration = _debatingPeriodDuration;
+    }
+
+    function deposit(uint256 _amount) external {
+        voteToken.safeTransferFrom(msg.sender, address(this), _amount);
+        _electors[msg.sender].balance = _amount;
+
+        emit Deposit(msg.sender, _amount);
+    }
+
+    function vote(uint256 _id, bool _support) external {
+        Elector storage user = _electors[msg.sender];
+        require(user.balance > 0, "You don't have tokens");
+
+        Proposal storage proposal = _proposals[_id];
+        require(!proposal.voted[msg.sender], "You've already done the voice");
+
+        // User can claim his own tokens after the end of the last proposal
+        if (proposal.finishAt > user.canClaimAt) user.canClaimAt = proposal.finishAt;
+
+        if (_support) proposal.pros += user.balance;
+        else proposal.cons += user.balance;
+
+        proposal.voted[msg.sender] = true;
+
+        emit Vote(_id, msg.sender, user.balance, _support);
+    }
+
+    function withdraw(uint256 _amount) external {
+        Elector storage user = _electors[msg.sender];
+        require(_amount > 0, "Amount should be greater than 0");
+        require(user.balance > 0, "You don't have tokens");
+        require(user.balance >= _amount, "Amount greater than your balance");
+        require(user.canClaimAt <= block.timestamp, "You can withdraw after the latest proposal");
+
+        user.balance -= _amount;
+        voteToken.safeTransfer(msg.sender, _amount);
+
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    function addProposal(bytes calldata _signature, address _recipient, string calldata _description) external onlyChairperson {
+        _voteCounter.increment();
+
+        uint256 id = _voteCounter.current();
+
+        _proposals[id].finishAt = block.timestamp + debatingPeriodDuration;
+        _proposals[id].description = _description;
+        _proposals[id].recipient = _recipient;
+        _proposals[id].active = true;
+        _callDataProposals[id] = _signature;
+    }
+
+    function finishProposal(uint256 _id) external {
+        Proposal storage proposal = _proposals[_id];
+        require(proposal.finishAt <= block.timestamp, "Debating period is not over");
+        require(proposal.active, "Debate is over");
+
+        delete proposal.active;
+
+        uint256 total = proposal.pros + proposal.cons;
+        if (
+            total < minimumQuorum ||
+            proposal.cons >= proposal.pros
+        ) {
+            emit FinishProposal(_id, proposal.pros, proposal.cons, total, false);
+            return;
+        }
+
+        (bool success,) = proposal.recipient.call(_callDataProposals[_id]);
+        emit FinishProposal(_id, proposal.pros, proposal.cons, total, success);
+    }
+
+
+    function setMinimumQuorum(uint256 _amount) external onlyOwner {
+        minimumQuorum = _amount;
+    }
+
+    function setDebatingPeriodDuration(uint256 _time) external onlyOwner {
+        debatingPeriodDuration = _time;
+    }
+}
---
+++ test/DAO.test.ts
@@ -0,0 +1,365 @@
+import { ethers, network } from 'hardhat';
+import chai, { expect } from 'chai';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+import { DAO, Token } from '../typechain';
+import { TokenInterface } from '../typechain/Token';
+
+chai.use(require('chai-bignumber')());
+
+async function increaseTime(time: number) {
+  await network.provider.send('evm_increaseTime', [time]);
+  await network.provider.send('evm_mine');
+}
+
+describe('DAO Contract', function () {
+  let dao: DAO;
+  let token: Token;
+  let iToken: TokenInterface;
+  let owner: SignerWithAddress;
+  let chairperson: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let clean: string;
+
+  const debatingPeriodDuration = 60 * 60 * 24 * 3;
+  const minimumQuorum = 5000;
+
+  before(async () => {
+    [owner, chairperson, addr1] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory('Token');
+    token = await Token.deploy();
+    const Dao = await ethers.getContractFactory('DAO');
+    dao = await Dao.deploy(
+      chairperson.address,
+      token.address,
+      minimumQuorum,
+      debatingPeriodDuration
+    );
+
+    iToken = <TokenInterface>Token.interface;
+
+    await token.transferOwnership(dao.address);
+    await token.transfer(addr1.address, minimumQuorum);
+
+    clean = await network.provider.send('evm_snapshot');
+  });
+  afterEach(async () => {
+    await network.provider.send('evm_revert', [clean]);
+    clean = await network.provider.send('evm_snapshot');
+  });
+
+  describe('Withdraw', () => {
+    it('should withdraw tokens', async () => {
+      const startBalance = await token.balanceOf(owner.address);
+
+      await token.approve(dao.address, minimumQuorum);
+
+      await dao.deposit(minimumQuorum);
+      await dao.withdraw(minimumQuorum);
+      const finalBalance = await token.balanceOf(owner.address);
+      expect(finalBalance).to.be.eq(startBalance);
+    });
+    it('should withdraw a part of tokens', async () => {
+      const startBalance = await token.balanceOf(owner.address);
+
+      await token.approve(dao.address, minimumQuorum);
+
+      await dao.deposit(minimumQuorum);
+      await dao.withdraw(minimumQuorum / 2);
+      await dao.withdraw(minimumQuorum / 2);
+      const finalBalance = await token.balanceOf(owner.address);
+      expect(finalBalance).to.be.eq(startBalance);
+    });
+    it('should decrease tokens', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.withdraw(minimumQuorum);
+
+      const tx = dao.vote(1, true);
+      const reason = "You don't have tokens";
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it('should revert if amount equals 0', async () => {
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+
+      const tx = dao.withdraw(0);
+      const reason = 'Amount should be greater than 0';
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it('should revert if amount greater than user balance', async () => {
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+
+      const tx = dao.withdraw(minimumQuorum * 2);
+      const reason = 'Amount greater than your balance';
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it('should revert if user has no deposited tokens', async () => {
+      const tx = dao.withdraw(minimumQuorum);
+      const reason = "You don't have tokens";
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it('should revert if the proposal is not finished', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, true);
+
+      const tx = dao.withdraw(minimumQuorum);
+      const reason = 'You can withdraw after the latest proposal';
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it("should revert if all proposals aren't finished", async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+
+      await increaseTime(debatingPeriodDuration / 2);
+
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+      await dao.vote(2, true);
+      await dao.vote(1, true);
+
+      await increaseTime(debatingPeriodDuration / 2);
+
+      const tx = dao.withdraw(minimumQuorum);
+      const reason = 'You can withdraw after the latest proposal';
+      await expect(tx).to.be.revertedWith(reason);
+    });
+  });
+  describe('Vote', () => {
+    it('can take part in several votes', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      const totalSupplyBefore = await token.totalSupply();
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, true);
+      await dao.vote(2, true);
+
+      await increaseTime(debatingPeriodDuration);
+
+      await dao.finishProposal(1);
+      await dao.finishProposal(2);
+
+      const totalSupplyAfter = await token.totalSupply();
+      expect(totalSupplyAfter).to.be.eq(totalSupplyBefore.add(5000 * 2));
+    });
+    it('should revert if user has no tokens', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      const tx = dao.vote(1, true);
+      const reason = "You don't have tokens";
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it('should revert if vote has already done', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, true);
+
+      const tx = dao.vote(1, true);
+      const reason = "You've already done the voice";
+      await expect(tx).to.be.revertedWith(reason);
+    });
+  });
+  describe('addProposal', () => {
+    it('should call only by chairperson', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      const tx = dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+      await expect(tx).not.to.be.reverted;
+    });
+    it('should revert if caller is not the chairperson', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      const tx = dao
+        .connect(addr1)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+      const reason = 'Caller is not the chairperson';
+      await expect(tx).to.be.revertedWith(reason);
+    });
+  });
+  describe("finishProposal", () => {
+    it('should execute call', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      const totalSupplyBefore = await token.totalSupply();
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, true);
+
+      await increaseTime(debatingPeriodDuration);
+
+      await dao.finishProposal(1);
+      const totalSupplyAfter = await token.totalSupply();
+      expect(totalSupplyAfter).to.be.eq(totalSupplyBefore.add(minimumQuorum));
+    });
+    it('should revert if debating period has not passed', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      const tx = dao.finishProposal(1);
+      const reason = 'Debating period is not over';
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it('should revert if proposal has already finished', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, true);
+
+      await increaseTime(debatingPeriodDuration);
+
+      await dao.finishProposal(1);
+      const tx = dao.finishProposal(1);
+      const reason = 'Debate is over';
+      await expect(tx).to.be.revertedWith(reason);
+    });
+    it('should fail proposal if votes lower than minimumQuorum', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum / 2);
+      await dao.deposit(minimumQuorum / 2);
+      await dao.vote(1, true);
+
+      await increaseTime(debatingPeriodDuration);
+
+      const tx = dao.finishProposal(1);
+      await expect(tx)
+        .to.be.emit(dao, 'FinishProposal')
+        .withArgs(1, minimumQuorum / 2, 0, minimumQuorum / 2, false);
+    });
+    it('should fail proposal if cons greater than pros', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, false);
+
+      await increaseTime(debatingPeriodDuration);
+
+      const tx = dao.finishProposal(1);
+      await expect(tx)
+        .to.be.emit(dao, 'FinishProposal')
+        .withArgs(1, 0, minimumQuorum, minimumQuorum, false);
+    });
+    it('should fail proposal if cons equals pros', async () => {
+      const callData = iToken.encodeFunctionData('mint', [dao.address, 5000]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, true);
+
+      await token.connect(addr1).approve(dao.address, minimumQuorum);
+      await dao.connect(addr1).deposit(minimumQuorum);
+      await dao.connect(addr1).vote(1, false);
+
+      await increaseTime(debatingPeriodDuration);
+
+      const tx = dao.finishProposal(1);
+      await expect(tx)
+        .to.be.emit(dao, 'FinishProposal')
+        .withArgs(1, minimumQuorum, minimumQuorum, minimumQuorum * 2, false);
+    });
+    it('should fail proposal if function call failed', async () => {
+      const callData = iToken.encodeFunctionData('transfer', [
+        dao.address,
+        minimumQuorum * 2,
+      ]);
+      await dao
+        .connect(chairperson)
+        .addProposal(callData, token.address, 'Increase TotalSupply');
+
+      await token.approve(dao.address, minimumQuorum);
+      await dao.deposit(minimumQuorum);
+      await dao.vote(1, true);
+
+      await increaseTime(debatingPeriodDuration);
+
+      const tx = dao.finishProposal(1);
+      await expect(tx)
+        .to.be.emit(dao, 'FinishProposal')
+        .withArgs(1, minimumQuorum, 0, minimumQuorum, false);
+    });
+  });
+  describe('Setters', () => {
+    describe('setMinimumQuorum', () => {
+      it('should change minimumQuorum', async () => {
+        const customValue = minimumQuorum * 2;
+        await dao.setMinimumQuorum(customValue);
+        const newValue = await dao.minimumQuorum();
+        expect(newValue).to.be.eq(customValue);
+      });
+      it('should revert if caller is not the owner', async () => {
+        const customValue = minimumQuorum * 2;
+        const tx = dao.connect(addr1).setMinimumQuorum(customValue);
+        const reason = 'Caller is not the owner';
+        await expect(tx).to.be.revertedWith(reason);
+      });
+    });
+    describe('setDebatingPeriodDuration', () => {
+      it('should change debatingPeriodDuration', async () => {
+        const customValue = debatingPeriodDuration * 2;
+        await dao.setDebatingPeriodDuration(customValue);
+        const newValue = await dao.debatingPeriodDuration();
+        expect(newValue).to.be.eq(customValue);
+      });
+      it('should revert if caller is not the owner', async () => {
+        const customValue = debatingPeriodDuration * 2;
+        const tx = dao.connect(addr1).setDebatingPeriodDuration(customValue);
+        const reason = 'Caller is not the owner';
+        await expect(tx).to.be.revertedWith(reason);
+      });
+    });
+  });
+});
